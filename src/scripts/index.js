93ccdc16a6a436f87d9ce6f98454aa58ea764d4f
kisnak21 <krisnastya21@gmail.com>
2022-10-23 10:23:28 +0700

    submission katalog restoran

---
+++ src/scripts/index.js
@@ -0,0 +1,20 @@
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
+
+const menu = document.querySelector("#menu");
+const hero = document.querySelector(".hero");
+const main = document.querySelector("main");
+const drawer = document.querySelector("#drawer");
+
+menu.addEventListener("click", function (event) {
+  drawer.classList.toggle("open");
+  event.stopPropagation();
+});
+
+hero.addEventListener("click", function () {
+  drawer.classList.remove("open");
+});
+
+main.addEventListener("click", function () {
+  drawer.classList.remove("open");
+});
